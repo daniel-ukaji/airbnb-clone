4e41b1ec7d9f254794dbb918aa3d27a9dce89600
Daniel Ukaji <dannyukaji@gmail.com>
2022-10-04 15:49:27 +0100

    First Commit.

---
+++ components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header>
+        <div>Header</div>
+    
+        {/* left */}
+        <div></div>
+
+        {/* Middle */}
+        <div></div>
+
+        {/* Right */}
+        <div></div>
+
+    </header>
+  )
+}
+
+export default Header

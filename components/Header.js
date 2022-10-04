d278219d3a69070b1e10f6bcd062522c03282b04
Daniel Ukaji <dannyukaji@gmail.com>
2022-10-04 20:16:05 +0100

    Navbar Commit Done.

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import Image from 'next/image'
-import { SearchIcon } from '@heroicons/react/solid'
+import { 
+  SearchIcon,
+  GlobeAltIcon,
+  UserCircleIcon,
+  UsersIcon,
+  MenuIcon,
+} from '@heroicons/react/solid'
 
 function Header() {
   return (
@@ -24,7 +30,15 @@ function Header() {
         </div>
 
         {/* Right */}
-        <div></div>
+        <div className='flex items-center space-x-4 justify-end text-gray-500'>
+          <p className='hidden md:inline cursor-pointer'>Become a Host</p>
+          <GlobeAltIcon className='h-6 cursor-pointer' />
+
+          <div className='flex items-center space-x-2 border-2 p-2 rounded-full'>
+            <MenuIcon className='h-6' />
+            <UserCircleIcon className='h-6' />
+          </div>
+        </div>
 
     </header>
   )
